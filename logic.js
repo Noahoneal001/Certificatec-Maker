a18510780ebd442fc46d76d82d634100a0ae4d3d
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-08 15:50:36 -0800

    Stuff

---
+++ logic.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById(`print`).addEventListener(`click`, function () {
+        window.print()
+    })
+
+    
+})

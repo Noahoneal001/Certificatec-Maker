15ba4446c12874cdcd7c47c8d3f45f6b3a575020
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-09 15:47:26 -0800

    update

--- logic.js
+++ logic.js
@@ -1,8 +1,14 @@
+let isA = [`Is Tall`, `Is A Skill Issue`, `Is FOR ROCK AND STONE!`]
+let logoList = [`.tall`, `.skill`, `.rockAndStone`]
+
 document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById(`userName`).addEventListener(`change`, function () {
+        
+    })
 
     document.getElementById(`print`).addEventListener(`click`, function () {
         window.print()
     })
 
-    
 })

b9794dde02d1ac520b343a788100edbcf4a04164
Noah O'Neal Home (CHROME OS) <noah.o'neal23@student.cart.org>
2024-02-09 19:41:58 -0800

    Test

--- logic.js
+++ logic.js
@@ -1,15 +1,32 @@
-let isA = [`Is Tall`, `Is A Skill Issue`, `Is FOR ROCK AND STONE!`]
-let logoList = [`.tall`, `.skill`, `.rockAndStone`]
+let isA = [`Is Tall`, `Is A Skill Issue`, `Is FOR ROCK AND STONE!`];
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById(`userName`).addEventListener(`change`, function () {
-        let Name = document.getElementById(`CertificateName`)
-        Name.innertext() = 
-    })
+    document.getElementById(`userName`).addEventListener(`change`, function (nameOutput) {
+        let Name = document.getElementById(`CertificateName`);
+        let CertificateNameOutput = nameOutput.target.value;
+        if (CertificateNameOutput.trim().length == 0) {
+            Name.innerText = `Name`
+        }
+        else {
+            Name.innerText = CertificateNameOutput
+        };
+    });
+
+    document.getElementById(`stylechose`).addEventListener(`change`, function (valueData) {
+        swaper(valueData.target.value);
+    });
 
     document.getElementById(`print`).addEventListener(`click`, function () {
-        window.print()
-    })
+        window.print();
+    });
 
-})
+    function swaper (valueSwaper) {
+        let sample = document.getElementById(`CertificateName`)
+        switch(valueSwaper) {
+            case `tall`:
+                if (sample.contains())
+        }
+    };
+
+});

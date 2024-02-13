4e5bd1c04758c4e99c933c7c6f642ee5b0f18623
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-13 14:06:19 -0800

    Done

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
 let isA = [`Is Tall`, `Is A Skill Issue`, `Is FOR ROCK AND STONE!`];
+let logoList = [`/Images/IconTall.png`, `/Images/iconSkillIssue.png`, `/Images/pickaxe.png`]
+let signerList = [`Baked Beans`, `Karl`]
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -21,11 +23,32 @@ document.addEventListener("DOMContentLoaded", function () {
         window.print();
     });
 
-    function swaper (valueSwaper) {
-        let sample = document.getElementById(`CertificateName`)
-        switch(valueSwaper) {
+    function swaper(valueSwaper) {
+        let sample = document.getElementById(`isWhat`)
+        let swapingTime = document.getElementById(`plsWork`)
+        let imgSwap = document.getElementById(`logo`)
+        let signer = document.getElementById(`signed`)
+        swapingTime.classList = `CertificateBox`
+        switch (valueSwaper) {
             case `tall`:
-                if (sample.contains())
+                swapingTime.classList.add(`font1`);
+                imgSwap.setAttribute(`src`, logoList[0]);
+                sample.innerText = isA[0];
+                signer.innerText = signerList[0]
+                break;
+            case `skill`:
+                swapingTime.classList.add(`font2`);
+                imgSwap.setAttribute(`src`, logoList[1]);
+                sample.innerText = isA[1];
+                signer.innerText = signerList[0]
+                break;
+            case `drg`:
+                swapingTime.classList.add(`font3`);
+                imgSwap.setAttribute(`src`, logoList[2]);
+                sample.innerText = isA[2];
+                signer.innerText = signerList[1]
+                break;
+
         }
     };
 
